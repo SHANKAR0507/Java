2963f331413a50def54c43b94d2eb34b1952cad8
Shankar0507 <shankarkakamari@gmail.com>
2019-09-10 15:38:31 +0530

    second

---
+++ First/src/Second.java
@@ -0,0 +1,19 @@
+
+public class Second {
+
+	public int p;
+	private String q;
+	
+	public int getP() {
+		return p;
+	}
+	public void setP(int p) {
+		this.p = p;
+	}
+	public String getQ() {
+		return q;
+	}
+	public void setQ(String q) {
+		this.q = q;
+	}
+}
